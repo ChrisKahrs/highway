532d1cc88a1209f1890abf3d9cd2f98e36f082b8
Chris Kahrs <chriskahrs@msn.com>
2022-10-26 14:32:46 -0400

    gifs

---
+++ highwaySB3_run2.py
@@ -0,0 +1,30 @@
+import gym
+import highway_env
+from stable_baselines3 import PPO, DQN
+# from pyvirtualdisplay import Display
+# display = Display(visible=0, size=(1400, 900))
+# display.start()
+
+env = gym.make("highway-fast-v0")
+env.metadata = {'render_modes': ['human', 'rgb_array'], 'render_fps': 10}
+env.reset(seed=42)
+env.configure({"offscreen_rendering": True})
+env = gym.wrappers.RecordVideo(env, "recording4", env.metadata)
+env.start_video_recorder()
+
+# Load and test saved model
+model = DQN.load("models/DQN9/run1")
+reward_sum = 0
+for i in range(1):
+  terminated = truncated = False
+  obs, info = env.reset(seed=42)
+  print("reward_sum: ", reward_sum)
+  reward_sum = 0
+  while not (terminated or truncated):
+    action, _states = model.predict(obs, deterministic=True)
+    action = int(action)
+    print("action: ", action)
+    obs, reward, terminated, truncated, info = env.step(action)
+    print("reward: ", reward)
+    reward_sum += reward
+    print(env.render())
--- highwaySB3_run3.py
+++ highwaySB3_run3.py
@@ -1,8 +1,6 @@
 import gym
 import highway_env
-import matplotlib.pyplot as plt
 from stable_baselines3 import PPO, DQN
-from moviepy.editor import ImageSequenceClip
 # from pyvirtualdisplay import Display
 # display = Display(visible=0, size=(1400, 900))
 # display.start()
@@ -14,22 +12,14 @@ env.reset()
 # env.configure({"offscreen_rendering": True})
 # env = gym.wrappers.RecordVideo(env, "recording4", env.metadata)
 # env.start_video_recorder()
-screen = env.render(mode='rgb_array')
-plt.imshow(screen)
-screens = []
-screens.append(screen)
-count = 0
 
 # Load and test saved model
 model = DQN.load("models/DQN9/run1")
 reward_sum = 0
-for i in range(3):
+for i in range(10):
   terminated = truncated = done = False
   obs = env.reset()
   print("reward_sum: ", reward_sum)
-  if count > 0:
-    clip = ImageSequenceClip(list(screens), fps=5)
-    clip.write_gif(f'test_{count}.gif', fps=15)
   reward_sum = 0
   while not done:
     action, _states = model.predict(obs, deterministic=True)
@@ -38,10 +28,4 @@ for i in range(3):
     obs, reward, done, info = env.step(action)
     # print("reward: ", reward)
     reward_sum += reward
-    # env.render()
-    screen = env.render(mode='rgb_array')
-    # print(obs)
-    screens.append(screen)
-    plt.imshow(screen)
-    count += 1
-
+    env.render()
