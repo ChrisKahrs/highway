a72a6dcc73ffdc1367ef180840a7ec05f991ea02
Chris Kahrs <chriskahrs@msn.com>
2022-10-27 19:40:56 -0400

    video

--- highwaySB3_run.py
+++ highwaySB3_run.py
@@ -1,24 +1,21 @@
 import gym
 import highway_env
-import matplotlib.pyplot as plt
 from stable_baselines3 import PPO, DQN
 from moviepy.editor import ImageSequenceClip
+from gym.wrappers.record_video import capped_cubic_video_schedule
 import pprint
-# from pyvirtualdisplay import Display
-# display = Display(visible=0, size=(1400, 900))
-# display.start()
 
+# Video Stuff? 
+    # from pyvirtualdisplay import Display
+    # display = Display(visible=0, size=(1400, 900))
+    # display.start()
+    # env.metadata = {'render_modes': ['rgb_array'], 'render_fps': 10}
+    # env.metadata['render_fps'] = 10
+    # env.configure({"offscreen_rendering": True})
+    # env = gym.wrappers.RecordVideo(env, "gifs/recording5", metadata?)
+    # env.start_video_recorder()
 
-config = {"observation": {
-          "type": "Kinematics",
-          "vehicles_count": 10,
-          "features": ["presence", "x", "y", "vx", "vy", "cos_h", "sin_h"],
-          "features_range": {
-            "x": [-100, 100],
-            "y": [-100, 100],
-            "vx": [-20, 20],
-            "vy": [-20, 20]
-          }}}
+# "features": ["presence", "x", "y", "vx", "vy", "cos_h", "sin_h"],
 
 config = {'action': {'type': 'DiscreteMetaAction'},
  'centering_position': [0.3, 0.5],
@@ -33,7 +30,7 @@ config = {'action': {'type': 'DiscreteMetaAction'},
  'manual_control': False,
  "observation": {
     "type": "Kinematics",
-    # "vehicles_count": 30,
+    "vehicles_count": 5,
     "features": ["presence", "x", "y", "vx", "vy"],
     "features_range": {
       "x": [-100, 100],
@@ -53,50 +50,33 @@ config = {'action': {'type': 'DiscreteMetaAction'},
  'screen_height': 150,
  'screen_width': 600,
  'show_trajectories': False,
- 'simulation_frequency': 15,
+ 'simulation_frequency': 5,
  'vehicles_count': 50,
- 'vehicles_density': 2}
+ 'vehicles_density': 1}
 
 env = gym.make("highway-v0", config=config)
-# pprint.pprint(env.config)
-
-env.metadata = {'render_modes': ['rgb_array'], 'render_fps': 10}
-env.metadata['render_fps'] = 10
 
 env.reset()
-# env.configure({"offscreen_rendering": True})
-# env = gym.wrappers.RecordVideo(env, "gifs/recording5")
-# env.start_video_recorder()
 env = gym.wrappers.RecordEpisodeStatistics(env)
-# screen = env.render(mode='rgb_array')
-# plt.imshow(screen)
-# screens = []
-# screens.append(screen)
 count = 0
 
 # Load and test saved model
-model = DQN.load("models/DQN9/run8")
+model = DQN.load("models/DQN9/run9")
 
-for i in range(1):
+for i in range(3):
   terminated = truncated = done = False
   obs, info = env.reset(seed=i)
   reward_sum = 0
   screens = []
   screen = env.render(mode='rgb_array')
-  # plt.imshow(screen)
   screens.append(screen)
   while (not truncated) and (not terminated):
     action, _states = model.predict(obs, deterministic=True)
     action = int(action)
-    # print("action: ", action)
     obs, reward, terminated, truncated, info = env.step(action)
-    # print("reward: ", reward)
     reward_sum += reward
-    # env.render()
     screen = env.render(mode='rgb_array')
-    # print(obs)
     screens.append(screen)
-    # plt.imshow(screen)
     count += 1
     if terminated or truncated:
       print("reward_sum: ", reward_sum)
